58a13f3e8b61924ab2fb8f9d31704f6fbb12888b
Eric Lau <yasuyo369@gmail.com>
2017-01-10 18:51:59 +0000

    old code not working

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,17 @@
-function basicTeenager(age) {
+function basicTeenager(age = 13) {
+if ((age > 12)&&(age < 20)){return"You are a teenager!";}
 
 }
 
-function teenager(age) {
-
+function teenager(age = 15) {
+if((age>12)&&(age<20)){return"You are a teenager!"}
+  else{return"You are not a teenager"}
 }
 
 function ageChecker(age) {
-
+if(){}
+else if () {}
+else(return"You are a grownup!";)
 }
 
 function ternaryTeenager(age) {
