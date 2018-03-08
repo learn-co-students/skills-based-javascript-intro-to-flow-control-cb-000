3664102468e8ce1f1813eddd6eb8b4246b8a2f1f
Jen Poole <contact.vandepool@gmail.com>
2018-03-08 04:50:46 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,8 @@
+// basicTeenager should return "You are a teenager!" if the age is between 13-19
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
