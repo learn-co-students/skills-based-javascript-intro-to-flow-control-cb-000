18f8ce16e4dfee26d0c90c463d003abfece233a1
Jen Poole <contact.vandepool@gmail.com>
2018-03-08 04:55:03 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,8 +15,17 @@ function teenager(age) {
   }
 }
 
+// ageChecker should return "You are a teenager!" if the age is between 13-19
+// ageChecker should return "You are a kid" if the age is 12 or below
+// ageChecker should return "You are a grownup" if the age is 20 or above
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
